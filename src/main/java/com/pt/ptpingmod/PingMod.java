d72cb8c9258b057cc46f5474bcaea7012c0e0236
PTense <126371042+PTense@users.noreply.github.com>
2025-07-06 16:38:43 -0700

    Estructura del mod inicial 2

--- src/main/java/com/pt/ptpingmod/PingMod.java
+++ src/main/java/com/pt/ptpingmod/PingMod.java
@@ -1,5 +1,17 @@
 package com.pt.ptpingmod;
 
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = PingMod.MODID, name = PingMod.NAME, version = PingMod.VERSION)
 public class PingMod {
 
+    public static final String MODID = "ptpingmod";
+    public static final String NAME = "PT Ping Mod";
+    public static final String VERSION = "1.0";
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event) {
+        System.out.println(NAME + " cargado correctamente.");
+    }
 }
